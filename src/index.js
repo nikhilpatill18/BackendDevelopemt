525e2265f8a7877a2482faab10b7c6dee429ec74
nikhilpatill18 <nikhilpatil6060@gmail.com>
2024-10-30 17:28:29 +0530

    cors cookies ans some utils files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 // require('dotenv').config({ path: './env' })
 import dotenv from 'dotenv'
 import connectdb from "./db/index.js";
+import app from './app.js';
 dotenv.config({
     path: './env'
 })
 connectdb()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`)
+        })
+    })
+    .catch((err) => {
+        console.log("data base connection error", err);
 
-/*  ; (async () => {
-     try {
-         await mongoose.connect(`${process.env.MONGODB_URI}/videotube`)
-         app.on("error", (err) => {
-             console.log("Error", err)
-         })
-         app.listen(process.env.PORT, () => {
-             console.log("welcome ti exopress")
-         })
-
-     }
-     catch (err) {
-         console.error("error", err)
-         throw err
-     }
- })() */
+    })
